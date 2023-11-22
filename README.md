d20165be0da46fc71da02de176a4b910e58e83c0
jsoldani <jsoldani@gmail.com>
2023-11-22 11:30:12 +0100

    first basic version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gfcadc
+# Green-flavoured, carbon-aware service
+To run the current app: 
+> python3 carbon-aware-service.py  
---
+++ carbon-aware-service.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from enum import Enum
+from flask import Flask
+
+# ------ STRATEGIES ------
+# Low power strategy
+class LowPowerStrategy():
+    def get():
+        return "Running low power.."
+
+# Full power strategy
+class FullPowerStrategy():
+    def get():
+        return "Running full power!!"
+
+# Enum the supported carbon-aware strategies
+class CarbonAwareStrategies(Enum):
+    LowPower = LowPowerStrategy
+    FullPower = FullPowerStrategy
+
+# ------ CONTEXT ------
+# Carbon-aware context
+class Context:
+    def getCarbonAwareStrategy():
+        # TODO: currently mocking co2 with a time check; to be fixed
+        current = datetime.now().second
+        if (current % 3 == 0):
+            return CarbonAwareStrategies.LowPower.value
+        else:
+            return CarbonAwareStrategies.FullPower.value
+
+# ------ SERVICE ------
+app = Flask(__name__)
+
+@app.route("/get")
+def get():
+    # TODO: add parameter extraction here, when needed
+    # Get carbon-aware strategy
+    strategy = Context.getCarbonAwareStrategy()
+    # Invoke strategy with dynamic typing
+    answer = strategy.get()
+    return answer
+
+app.run(port=5000)
