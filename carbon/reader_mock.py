18b4b42295f42f4c4b576e46f654038d2219f021
jsoldani <jsoldani@gmail.com>
2023-11-28 14:11:36 +0100

    sinusoidal mock

--- carbon/reader_mock.py
+++ carbon/reader_mock.py
@@ -4,13 +4,20 @@ class CarbonIntensityReader():
         return super().__new__(cls)
     
     # initializer (values are in grams of co2 per hour)
-    def __init__(self,startingValue,increaseValue,limitValue):
-        self.co2 = startingValue
-        self.step = increaseValue
-        self.limit = limitValue
+    def __init__(self,lowerbound,step,upperbound):
+        self.co2 = self.lowerbound = lowerbound
+        self.step = step
+        self.direction = +1
+        self.upperbound = upperbound
     
-    # mocked periodic carbon emissions 
+    # mocked carbon emissions' reader
     def read(self) -> int:
-        co2 = self.co2
-        self.co2 = (self.co2 + self.step) % self.limit
-        return co2
+        # get current value
+        co2 = self.co2 
+        # update current value (sinusoid going from lowerbound to upperbound - and back, each time increasing/decreasing by step)
+        if self.co2 <= self.lowerbound: 
+            self.direction = +1
+        if self.co2 >= self.upperbound: 
+            self.direction = -1
+        self.co2 += self.direction*self.step
+        return self.co2
