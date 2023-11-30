f1209cf4b82d8e209a792666b5cec1c0ac574723
jsoldani <jsoldani@gmail.com>
2023-11-30 11:01:13 +0100

    renaming

---
+++ flavours/high_power.py
@@ -0,0 +1,14 @@
+from flavours.interface import CarbonAwareStrategy
+
+# High power strategy
+class HighPowerStrategy(CarbonAwareStrategy):
+    
+    def nop() -> str:
+        return "HIGH_POWER"
+    
+    def avg(data) -> float:
+        sum = 0
+        size = len(data)
+        for i in range(0,size):
+            sum += data[i]
+        return sum/size
