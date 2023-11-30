1712ec1655fcad20f39817bab99240a3f8328687
jsoldani <jsoldani@gmail.com>
2023-11-30 10:03:02 +0100

    minor fix

--- carbon/reader_mock.py
+++ carbon/reader_mock.py
@@ -12,12 +12,10 @@ class CarbonIntensityReader():
     
     # mocked carbon emissions' reader
     def read(self) -> int:
-        # get current value
-        co2 = self.co2 
         # update current value (sinusoid going from lowerbound to upperbound - and back, each time increasing/decreasing by step)
         if self.co2 <= self.lowerbound: 
             self.direction = +1
         if self.co2 >= self.upperbound: 
             self.direction = -1
         self.co2 += self.direction*self.step
-        return co2
+        return self.co2
