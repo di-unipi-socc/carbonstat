c40f7625434d399db8c404039ef47a3ab25faa02
jsoldani <jsoldani@gmail.com>
2023-11-30 16:49:27 +0100

    increased entropy

--- data/generate.py
+++ data/generate.py
@@ -1,18 +1,21 @@
 import random
+import numpy as np
 
 # configuration for random number generation
-maxN = 1000000000
+maxN = 10000
 rand = lambda min,max : random.SystemRandom().randint(min,max)
 size = 1000000
 
 # generate random numbers
 values = []
 for i in range(size):
-    pivot = rand(0,1) # used to obtain high stdev
-    if pivot:
-        values.append(rand(1,round(maxN/6)))
-    else:
-        values.append(rand(round(5*maxN/6),maxN))
+    chunks = 4 # used to obtain high stdev
+    pivot = rand(0,chunks)**(i%5)%chunks 
+    start = round(pivot*maxN/chunks)
+    end = round((pivot+1)*maxN/chunks) 
+    values.append(rand(start,end))
+
+print(np.average(values),np.std(values))
 
 # generation of random numbers in file
 with open("numbers.txt","w") as numbers:
