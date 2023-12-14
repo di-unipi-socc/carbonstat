edb45fa8d315da603e974c6d5e3c16a47123e119
Stefano <stefano.forti92@gmail.com>
2023-12-14 22:30:30 +0100

    new version

--- policy_maker/clp/policymaker.pl
+++ policy_maker/clp/policymaker.pl
@@ -1,13 +1,14 @@
 :- use_module(library(clpfd)).
+:- set_prolog_flag(answer_write_options,[max_depth(0)]).
 :- consult('examples/example2.pl').
 
 %%% Main %%%
-go(DesiredP, Best) :-
-    init(Strategies, Times), minPrecision(DesiredP, MinP),
+go(DesiredP, (EF,PF,SF)) :-
+    init(Strategies, Times), minPrecision(DesiredP, MinP,TotReqs),
     Strategies ins 0..2, % strategies are numbered from 0 (hp) to 2 (lp)
-    P #>= MinP, % precision is at least MinP
+    P #>= MinP,          % precision is at least MinP
     time(findall((E,P,S), solution(Times, S, E, P), Sols)),
-    sort(Sols, [Best|_]), write(Best), nl.
+    sort(Sols, [(EF,PI,SF)|_]), PF is PI / TotReqs. % sort solutions by increasing emissions, take first one
 
 solution(Times, Strategies, Emissions, Precision) :-
     maplist(selectStrategy, Times, Strategies, Precisions), 
@@ -19,17 +20,14 @@ selectStrategy(Time, Strategy, P) :-
     strategy(Strategy, _, _), requestRate(Time, R), precision(R, Strategy, P).
 
 thresholdCoherent(Times, Strategies) :-
-    \+ (    
-            member(T1,Times), member(T2,Times), dif(T1,T2), 
+    \+ (    member(T1,Times), member(T2,Times), dif(T1,T2), 
             nth1(T1, Strategies, S1), nth1(T2, Strategies, S2), S1 < S2, 
             carbonIntensity(T1,C1), carbonIntensity(T2,C2), 
-            C1 >= C2
-        ).
+            C1 >= C2    ).
 
 precision(R, Strategy, Precision) :- strategy(Strategy, _, P), Precision is R * P.
 
-emissions(Solution, TotalEmissions) :-
-    emissions(Solution, 1, Emissions), sumlist(Emissions, TotalEmissions).
+emissions(Solution, Emissions) :- emissions(Solution, 1, Es), sumlist(Es, Emissions).
 
 emissions([S|Ss], I, [E|Es]) :-
     strategy(S, Duration, _), requestRate(I, R), maxTime(MaxTime),
@@ -47,4 +45,4 @@ init(Strategies, Times) :- maxTime(M), initLists(M, Strategies, Ts), reverse(Ts,
 initLists(N, [_|Ns], [N|Ts]) :- N > 0, N1 is N-1, initLists(N1, Ns, Ts).
 initLists(0, [], []).
 
-minPrecision(DesiredP,MinP) :- findall(R, requestRate(_, R), Rs), sumlist(Rs, TotReqs), MinP is TotReqs * DesiredP.
+minPrecision(DesiredP,MinP,TotReqs) :- findall(R, requestRate(_, R), Rs), sumlist(Rs, TotReqs), MinP is TotReqs * DesiredP.
